83b1d712adaccc6362fd49a77de453c02f7c6082
gadyg <gadyg@LAPTOP-HPDPS9SA>
2021-02-25 19:30:35 +0200

    Initial commit

---
+++ CTS_seminar1_25_02_2021_15.30/src/program/Main.java
@@ -0,0 +1,28 @@
+package program;
+
+import clase.Giraffe;
+import clase.Zebra;
+import clase.Zoo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Zoo z = new Zoo();
+		Giraffe giraffe = new Giraffe("Girafa1");
+		Giraffe giraffe2 = new Giraffe("Girafa2");
+		
+		z.addAnimal(giraffe);
+		z.addAnimal(giraffe2);
+		
+		z.feedAllAnimals();
+		
+		Zebra z1 = new Zebra("z1");
+		Zebra z2 = new Zebra("z2");
+		
+		z.addAnimal(z1);
+		z.addAnimal(z2);
+		
+		z.feedAllAnimals();
+	}
+
+}
