d4a033dedd8b63cf53bbcea0922294bf969db6ac
gadyg <gadyg@LAPTOP-HPDPS9SA>
2021-02-25 18:57:35 +0200

    Adding classes Animal, Girafa, Zoo, Zookeeper

---
+++ CTS_seminar1_25_02_2021_17.30/src/clase/Zoo.java
@@ -0,0 +1,35 @@
+package clase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zoo {
+	private Zookeeper zookeeper;
+	private List<Animal> animalList;
+	
+	
+	
+	public Zoo() {
+		animalList = new ArrayList<>();
+		zookeeper = new Zookeeper("Ion");
+	}
+
+
+
+	public Zoo(Zookeeper zookeeper, List<Animal> animalList) {
+		super();
+		this.zookeeper = zookeeper;
+		this.animalList = animalList;
+	}
+	
+	public void adaugaAnimal(Animal animal) {
+		animalList.add(animal);
+	}
+	
+	public void feedAllAnimals() {
+		for(Animal animal:animalList ) {
+			zookeeper.feed(animal);
+		}
+	}
+	
+}
--- CTS_seminar1_25_02_2021_17.30/src/program/Main.java
+++ CTS_seminar1_25_02_2021_17.30/src/program/Main.java
@@ -1,9 +1,22 @@
 package program;
 
+import clase.Girafa;
+import clase.Zoo;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Zoo zoo = new Zoo();
+		
+		Girafa girafa1 = new Girafa("girafa1");
+		Girafa girafa2 = new Girafa("girafa2");
+		
+		zoo.adaugaAnimal(girafa1);
+		zoo.adaugaAnimal(girafa2);
+		
+		zoo.feedAllAnimals();
+		
+				
 
 	}
 
